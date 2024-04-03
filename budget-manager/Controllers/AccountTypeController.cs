29566796787dcb75e10657f899284d049eec0df0
nosmow <nosmow.dev@gmail.com>
2024-04-03 07:10:53 -0500

    Add funcionality to create account types

---
+++ budget-manager/Controllers/AccountTypeController.cs
@@ -0,0 +1,24 @@
+﻿using budget_manager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace budget_manager.Controllers
+{
+    public class AccountTypeController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AccountType accountType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(accountType);          
+            }
+
+            return View();
+        }
+    }
+}
